f04800aad2262b6bd61e4a45da2ba608ba7d0ea8
biankkorban <bianka.korban@gmail.com>
2020-06-15 16:57:26 +1000

    Updates

---
+++ precache-manifest.8f55c5b62f5106c470d67071f015a2d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b4b2ed3421a1c6467a3c7a7613d2fc0",
+    "url": "/social-card-built-with-React/index.html"
+  },
+  {
+    "revision": "648c99318a7c26bee227",
+    "url": "/social-card-built-with-React/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e75a3cb4cfb1a530edfc",
+    "url": "/social-card-built-with-React/static/css/main.19ecf4a6.chunk.css"
+  },
+  {
+    "revision": "648c99318a7c26bee227",
+    "url": "/social-card-built-with-React/static/js/2.c848ad36.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/social-card-built-with-React/static/js/2.c848ad36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75a3cb4cfb1a530edfc",
+    "url": "/social-card-built-with-React/static/js/main.e9bb81d7.chunk.js"
+  },
+  {
+    "revision": "8aac6f678bb4da83dc73",
+    "url": "/social-card-built-with-React/static/js/runtime-main.a5ffad9a.js"
+  },
+  {
+    "revision": "154a05bec5312dc3cd11563da0b27e65",
+    "url": "/social-card-built-with-React/static/media/Sydney.154a05be.jpg"
+  },
+  {
+    "revision": "8d4f50f5a48782110cc27afa126ef828",
+    "url": "/social-card-built-with-React/static/media/profile_img.8d4f50f5.jpg"
+  }
+]);
